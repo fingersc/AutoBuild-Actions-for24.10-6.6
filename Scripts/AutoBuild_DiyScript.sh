4961ffc8addec274a2a8249a0991d934d5dd12e6
fingersc <156872602+fingersc@users.noreply.github.com>
2025-07-20 15:22:51 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -33,6 +33,27 @@ Firmware_Diy_Core() {
 Firmware_Diy() {
 
 	# 请在该函数内定制固件
+ 
+ 	# 删除部分原软件包，以保证可以后续添加的软件包可以被编译
+	#==========================================================
+	# smartdns
+	rm -rf feeds/packages/net/smartdns
+	rm -rf feeds/luci/applications/luci-app-smartdns
+	# UnblockNeteaseMusic
+	rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
+
+	# 添加额外软件包
+	#===========================================================
+	# smartdns
+	git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns/
+	git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns/
+	# lucky
+	git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git lucky-repo
+	cp -rf lucky-repo/luci-app-lucky package/luci-app-lucky
+	cp -rf lucky-repo/lucky package/lucky
+	# UnblockNeteaseMusic
+	git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git feeds/luci/applications/luci-app-unblockneteasemusic
+
 
 	# 可用预设变量, 其他可用变量请参考运行日志
 	# ${OP_AUTHOR}			OpenWrt 源码作者
